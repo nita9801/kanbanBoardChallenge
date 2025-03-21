ae411b5935dffbbf6db48b18da17abbca5643b11
nita9801 <187074331+nita9801@users.noreply.github.com>
2025-03-21 12:55:12 -0400

    Revert " updated the code and debugged now it runs with out styling but it Runs"
    This reverts commit dd936f150399a8c0e12216faf318bd9f533a393e.

--- source/App.jsx
+++ source/App.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from '@/Components/Header.jsx';
-import Footer from '@/Components/Footer.jsx';
-import Home from '@/pages/AboutMe.jsx';
-import Portfolio from '@/pages/Portfolio.jsx';
-import Contact from '@/pages/Contact.jsx';
-import Resume from '@/pages/Resume.jsx';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className="main-content">
-      <Routes>
-          <Route path="/home" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <main>
+        <Switch>
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
       </main>
-      <div className="footer-container">
-        <Footer />
-      </div>
+      <Footer />
     </Router>
   );
 };
-
 export default App;
