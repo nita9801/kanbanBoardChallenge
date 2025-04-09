16ed7cb7a31842e916476d40132685270584f12d
nita9801 <187074331+nita9801@users.noreply.github.com>
2025-04-08 20:26:48 -0400

    added the client code for login

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -1,11 +1,12 @@
-import axios, { isAxiosError } from 'axios';
-import type { AxiosError } from 'axios';
+
 import { UserLogin } from '../interfaces/UserLogin';
 
 interface LoginResponse {
   token: string;
   user: { id: number; username: string };
 }
+
+
 
 const login = async (userInfo: UserLogin): Promise<LoginResponse> => {
   const API_URL = 'http://localhost:3001/api/auth/login';
@@ -19,12 +20,7 @@ const login = async (userInfo: UserLogin): Promise<LoginResponse> => {
     // Return the response data
     return response.data;
   } catch (error) {
-    if (isAxiosError(error)) {
-      // Handle Axios-specific errors
-      const axiosError = error as AxiosError<{ message: string }>;
-      const errorMessage = axiosError.response?.data.message || 'Login failed';
-      throw new Error(errorMessage);
-    }
+    
     // Handle other unknown errors
     throw new Error('An unknown error occurred');
   }
